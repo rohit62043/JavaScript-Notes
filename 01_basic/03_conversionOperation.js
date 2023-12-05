1ee286013f2550cfdfeaa6d9aec59a62e3364272
Rohit Kumar Singh <100895939+rohit62043@users.noreply.github.com>
2023-12-05 12:08:32 +0000

    Conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score="123ab"
+
+console.log(typeof(score));
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);  //NaN
+//Be carefull while conversion in number it may give NaN
+// It will convert
+ //Null to 0
+ //undefine to NaN
+ //false to 0
+
+ /*
+ String to number conversion using Number(--);
+ "33"==>33
+ "3asd"==>NaN
+ false==>0
+ true==>1
+ */
+
+ // Number()-->to Number Conversion
+ /* Boolean()---> to boolean conversion
+    ""-->false
+    "fgfdg"-->true
+    1-->true
+    0-->false;
+ */
+
+
+//String()--> to String conversion
+//   let someNumber=33
+
+//   let stringNumber=String(someNumber);
+//   console.log(stringNumber);
+//   console.log(typeof(stringNumber));
+
+
+
+
