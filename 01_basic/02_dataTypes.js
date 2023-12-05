a01ab7982a865574c5cffd29bd6e3929272c343f
Rohit Kumar Singh <100895939+rohit62043@users.noreply.github.com>
2023-12-05 06:26:15 +0000

    dataTypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treate all JS code as newer version
+
+//alert(3+3); we are using node.js not browser
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean
+//null => standalone value
+let state=null //Khali hai not 0
+//undefine
+let fxnState; //the value is undefine i.e. we have not define
+let fxn=undefined; //we can also assign undefine
+
+//symbol=> mostly used in react to make components unique
+//object
+
+console.log(typeof "rohit");
+console.log(typeof undefined); //undefine
+console.log(typeof null); //object
