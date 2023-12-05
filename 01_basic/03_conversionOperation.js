586dbb1f5631408b3719319cd4d74a7290b4f517
Rohit Kumar Singh <100895939+rohit62043@users.noreply.github.com>
2023-12-05 12:21:50 +0000

    Operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,9 +1,9 @@
 let score="123ab"
 
-console.log(typeof(score));
+//console.log(typeof(score));
 let valueInNumber = Number(score);
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);  //NaN
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber);  //NaN
 //Be carefull while conversion in number it may give NaN
 // It will convert
  //Null to 0
@@ -34,6 +34,22 @@ console.log(valueInNumber);  //NaN
 //   console.log(stringNumber);
 //   console.log(typeof(stringNumber));
 
+//**************OPERATIONS*********/
+let str1="Rohit";
+let str2=" Singh";
+// console.log(str1-str2) ==> NaN
+// console.log(str1+str2) ==> Rohit Singh
 
+//***Tricky */
+console.log('2'+1); //==>21'
+console.log(1+'2'); //==>'12'
+console.log('2'+1+3); //==>'213'
+console.log(1+3+'2'); //==>'42'
+
+console.log(+true); //==>1
+console.log(+"");  //==>0
+
+//**prfix and postfix operator */
+//study from mdn
 
 
