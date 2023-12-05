87976a5c42282f1c12abe187874d8343a31c6de0
Rohit Kumar Singh <100895939+rohit62043@users.noreply.github.com>
2023-12-05 13:21:08 +0000

    Data type summary

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -18,3 +18,42 @@ let fxn=undefined; //we can also assign undefine
 console.log(typeof "rohit");
 console.log(typeof undefined); //undefine
 console.log(typeof null); //object
+
+//**How data are store and access from memory it is divided into 2 types---> primitive  and non-primitive */
+//Primitive--> Call by value
+//7 type: String, Number ,Boolean,null,undefine,symbol,BigInt
+//Non-primitive(Refrence type)-->Array,Object,function
+//JavaScript is Dynamically typed langauge
+// which means that data types of variables are determined by the value
+// they hold at runtime and can change throughout the program as we assign different values to them.
+
+//symbol-->provide uniquness-->Returns a new unique Symbol value.
+let id1=Symbol(123);
+let id2=Symbol(123);
+console.table([id1,id2]);
+console.log(id1===id2); // both are diffrent
+
+//BigInt
+let bigNumber=1234n;
+console.log(bigNumber);
+console.log(typeof(bigNumber));
+
+//Array,objecct,function
+const hero=["rohit",1,"453"]; //Array
+let obj={
+    name:"rohit",
+    age:21
+}
+
+const myFunction=function () {
+    console.log("Hello World");
+}
+
+console.table([typeof(hero),typeof(obj),typeof(myFunction)]);
+// ┌─────────┬────────────┐
+// │ (index) │   Values   │
+// ├─────────┼────────────┤
+// │    0    │  'object'  │
+// │    1    │  'object'  │
+// │    2    │ 'function' │-->object function
+// └─────────┴────────────┘
