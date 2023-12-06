c3b8ad28a8df69a78c95001b8aa5cd54f4fbdde2
Rohit Kumar Singh <100895939+rohit62043@users.noreply.github.com>
2023-12-06 16:28:24 +0000

    String

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,8 @@
+const name="Rohit";
+const repoCount=50;
+
+//Old method
+console.log("My name is "+name+" and my repoCount is "+repoCount);
+
+//New method
+console.log(`My name is ${name} and my repoCount is ${repoCount}`);
