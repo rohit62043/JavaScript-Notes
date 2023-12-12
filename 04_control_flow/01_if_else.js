79f6d84a3220850e8e8cdc35c16089a935a043d0
Rohit Kumar Singh <100895939+rohit62043@users.noreply.github.com>
2023-12-12 12:31:01 +0000

    if else

---
+++ 04_control_flow/01_if_else.js
@@ -0,0 +1,9 @@
+if(2!==3){
+    console.log("Executed.");
+}
+
+// <,>,>=,<=,==,!=,===,!==
+
+const balance=5000;
+if(balance>500) console.log("test"),console.log("test 2");
+else console.log("Test fail");
