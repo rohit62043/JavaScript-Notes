857ff24f0a111a436f59256fa741dc7484e46b3d
Rohit Kumar Singh <100895939+rohit62043@users.noreply.github.com>
2023-12-12 12:26:07 +0000

    Execution Context

---
+++ 03_basics/06_Execution_Context.js
@@ -0,0 +1,5 @@
+//Study from "https://www.freecodecamp.org/news/how-javascript-works-behind-the-scene-javascript-execution-context/#:~:text=When%20the%20JavaScript%20engine%20scans,for%20the%20variables%20and%20functions."
+
+// 1.Global Execution Context
+//2.Memmory Phase
+//3.Execution Phase
