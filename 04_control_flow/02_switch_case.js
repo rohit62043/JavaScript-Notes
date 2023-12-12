acf329d438bf390cd93b07b31f002d7aa716629a
Rohit Kumar Singh <100895939+rohit62043@users.noreply.github.com>
2023-12-12 12:33:36 +0000

    Switch Case

---
+++ 04_control_flow/02_switch_case.js
@@ -0,0 +1,20 @@
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+
+    default:
+        break;
+}
