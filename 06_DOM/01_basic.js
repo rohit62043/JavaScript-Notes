89c9067b3081fa18ea07b974a91fe80ed5d5bea1
Rohit Kumar Singh <100895939+rohit62043@users.noreply.github.com>
2023-12-19 05:23:39 +0000

    basic

---
+++ 06_DOM/01_basic.js
@@ -0,0 +1,9 @@
+// Diffrence between innerHTML , innerText , innerContent
+//HTMLCollection(no looping fxn),NodeList(only forEach) and Array they are similar but not same-->They  can be converted into array using Array.from(--);
+
+// const allH2=document.querySelectorAll('h2')
+// allH2.forEach((li)=>{
+//     li.style.color="pink";
+//      li.style.backgroundColor="blue";
+//    li.innerHTML="<h1>Rohit</h1>"
+// })
