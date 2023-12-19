e1e6ec2cbdffff2f52debbf3d9f4d23c46050764
Rohit Kumar Singh <100895939+rohit62043@users.noreply.github.com>
2023-12-19 07:54:41 +0000

    project 1 done

--- 07_Project/bg-changer/script.js
+++ 07_Project/bg-changer/script.js
@@ -1,7 +1,24 @@
-const buttons=document.querySelectorAll(".button");
-buttons.forEach((button)=>{
-    document.addEventListener('click',(e)=>{
-        const color=e.target.id;
-        document.body.style.backgroundColor=color;
-    })
-})
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+    
+  });
+});
+
