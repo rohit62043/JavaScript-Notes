e812239b100549635912839526c7091062001d18
Rohit Kumar Singh <100895939+rohit62043@users.noreply.github.com>
2023-12-26 15:43:11 +0000

    promoise

---
+++ 09_Advance_01/promise.js
@@ -0,0 +1,6 @@
+// asynchronous programming karne ke 2 tarika hai--> acync await or promise
+//Promise
+//Jyadatar hum promise consuume hi karta hai(like jab fetch call karte hai etc)
+//kabhi kabhi bnyenge bhi
+//Promise is an object representing the eventual completion or failure of an asynchronous operation
+//Promise has three state-->pending,fulfill,reject
