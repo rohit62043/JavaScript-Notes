bb5a1c038bb8389ce4926fc246e38c667659ee5b
Rohit <singhrohit51967@gmail.com>
2024-01-03 15:18:39 +0530

    Project 3 Done

--- 01_basic/07_Dates_in_js.js
+++ 01_basic/07_Dates_in_js.js
@@ -1,5 +1,5 @@
-let myDate=new Date()
-console.log(myDate,typeof(myDate));
+let myDate = new Date()
+console.log(myDate, typeof (myDate));
 console.log(myDate.toString());
 console.log(myDate.toDateString());
 console.log(myDate.toLocaleDateString());
@@ -9,25 +9,25 @@ console.log(myDate.toLocaleDateString());
 // let myCreatedDate2 = new Date(2023,0,23,5,3) //month start from 0
 // console.log(myCreatedDate2.toLocaleString());
 
-let myCreatedDate3= new Date("2023-01-22")//yyyy-mm-dd month start from 1
-let myCreatedDate4= new Date("2023-01-22")//dd-mm-yy month start from 1
+let myCreatedDate3 = new Date("2023-01-22")//yyyy-mm-dd month start from 1
+let myCreatedDate4 = new Date("2023-01-22")//dd-mm-yy month start from 1
 console.log(myCreatedDate3.toDateString());
 console.log(myCreatedDate4.toDateString());
 
 //timestamp
-let myTimeStamp=Date.now();
+let myTimeStamp = Date.now();
 
 console.log(myTimeStamp);
 console.log(myCreatedDate3.getTime());
 console.log(myCreatedDate3.getDate());
 console.log(myCreatedDate3.getHours());
-console.log(myCreatedDate3.getMonth()+1);
-console.log(Math.floor(Date.now()/1000));
+console.log(myCreatedDate3.getMonth() + 1);
+console.log(Math.floor(Date.now() / 1000));
 
-let newDate=new Date();
+let newDate = new Date();
 console.log(`Today is ${newDate.getFullYear()}`);
 
-console.log(newDate.toLocaleString('default',{
-    weekday:"long",
-    day:"2-digit"
+console.log(newDate.toLocaleString('default', {
+    weekday: "long",
+    day: "2-digit"
 }));
